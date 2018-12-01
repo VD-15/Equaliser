013d1f5753096e74ba8671832aa55fc312d2e048
Adzwoolly <adamwoollen@gmail.com>
2018-12-01 18:22:20 +0000

    Implement Intersects method

--- Equaliser/src/Engine/Collider.cs
+++ Equaliser/src/Engine/Collider.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,7 +10,19 @@ namespace Equaliser.Engine
 		private Vector2 size;
 		private Transform transform;
 
-		public bool Intersects(Collider collider) {
+		public bool Intersects(Collider collider)
+		{
+			if (this.transform.translation.X + (this.size.X / 2) >= collider.transform.translation.X - (collider.size.X / 2))
+			{
+				//
+			}
+
+            if (System.Math.Abs(this.transform.translation.X - collider.transform.translation.X) < (this.size.X / 2) + (collider.size.X / 2)
+                && System.Math.Abs(this.transform.translation.Y - collider.transform.translation.Y) < (this.size.Y / 2) + (collider.size.Y / 2))
+            {
+                return true;
+            }
+
 			return false;
 		}
 
