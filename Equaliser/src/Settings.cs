4720a4cb22ade3c06a9f6e2044d2e22f85c95f19
VD-15 <valkyriedev15@gmail.com>
2018-12-02 15:48:09 +0000

    added global to-do list

--- Equaliser/src/Settings.cs
+++ Equaliser/src/Settings.cs
@@ -11,5 +11,12 @@ namespace Equaliser
 		public static readonly Keys MOVE_LEFT = Keys.A;
 		public static readonly Keys MOVE_RIGHT = Keys.D;
         public static readonly Keys ATTACK = Keys.Space;
-    }
+	}
+
+	//This is the global to-do list, put TODO's here if they don't have anywhere else to go in the code
+	//TODO: Rectangle Colliders
+	//TODO: Box Colliders (Rectangle colliders but with rotation)
+	//TODO: Circle Colliders
+	//TODO: Ray Colliders
+	//TODO: Acceleration-based player movement
 }
